c1a81cf118f5f0bb3c3bed177a940b4b4d7d4825
Derek Ensign <derekensign@gmail.com>
2021-04-19 17:12:20 -0700

    initial setup

---
+++ craigslist-react/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Card extends React.Component {
+  render() {
+    return <h2>Hi, I am a Card!</h2>;
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+export default Card;

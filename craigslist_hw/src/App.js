e8f9025e97757021f27c0469509d35883ed99baf
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-20 00:07:17 -0400

    stretch goals done

--- craigslist_hw/src/App.js
+++ craigslist_hw/src/App.js
@@ -1,51 +1,24 @@
 import './App.css';
 import Post from './components/post'
+import data from './data'
 
 function App() {
   return (
     <div className="container">
-        <Post 
-        price = "$4500"
-        date = "Aug 28"
-        image = "3mK8RCf.png"
-        link = "https://imgur.com/gallery/UpzZDoW"
-        title = "STUNNING AND RENOVATED IN SOUTH END!"
-        info = "3 br - (South End)" />
-        <Post 
-        price = "$1625"
-        date = "Aug 28"
-        image = "8tSkIjv.png"
-        link = "https://imgur.com/t/mmorpg/vPfy2Te"
-        title = "Studio, Student Friendly, NO FEE"
-        info = "(Brighton)" />
-        <Post 
-        price = "$2250"
-        date = "Aug 28"
-        image = "4PrV0m1.png"
-        link = "https://imgur.com/r/Mid_Century/XBOdii5"
-        title = "Pet Friendly and Lovely Bay Windows"
-        info = "1 br - (Back Bay)" />
-        <Post 
-        price = "$2800"
-        date = "Aug 28"
-        image = "5xCusSA.png"
-        link = "https://imgur.com/t/interiordesign/SIUQkId"
-        title = "Amazing 2 bed / 1 bath in Brookline!"
-        info = "2 br - (Brookline)" />
-        <Post 
-        price = "$2550"
-        date = "Aug 28"
-        image = "AG3AnPP.png"
-        link = "https://imgur.com/r/RoomPorn/TNbgXwW"
-        title = "2 Bd, NO FEE, Laundy in Building, New/Renovated Kitchen, Dishwasher"
-        info = "2 br - (Brookline)" />
-        <Post 
-        price = "$3750"
-        date = "Aug 28"
-        image = "vrJjwU8.png"
-        link = "https://imgur.com/r/RoomPorn/tFfLW07"
-        title = "Pet Friendly and Lovely Bay Windows"
-        info = "3 br - (Beacon Hill)" />
+        {data.map((data, i) => {
+          return <Post 
+          key={i}
+          price = {data.price}
+          image = {data.image}
+          date = {data.postedOn}
+          link = {data.linkTo}
+          title = {data.title}
+          bedrooms = {data.beds}
+          size = {data.sqft}
+          location = {data.location}
+
+          />
+        })}
       </div>
   );
 }
--- craigslist_hw/src/components/post.js
+++ craigslist_hw/src/components/post.js
@@ -2,12 +2,14 @@ const Post = (props) => {
     return (
 <div className="postContainer">
     <span className="price">{props.price}</span>
-    <img className="image"src={`https://i.imgur.com/${props.image}`}/>
+    <img className="image"src={props.image}/>
     <div className="description">
         <span className="date">{props.date}</span>
         <a href={props.link} className="title">{props.title}</a>
         <span className="priceDown">{props.price}</span>
-        <p className="info">{props.info}</p>
+        {props.bedrooms !== 0 ? `${props.bedrooms}br - ` : null}
+        {props.size ? `${props.size}sqft - ` : null}
+        <p className="location">{props.location}</p>
     </div>
 </div>  
     )

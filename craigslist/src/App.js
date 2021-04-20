89f8766ac89631f5a446fbf45a16006bf886a71b
Tim Mok <timothyjmok@gmail.com>
2021-04-19 21:45:37 -0400

    core done, stretch 1-4 done, css done

--- craigslist/src/App.js
+++ craigslist/src/App.js
@@ -6,18 +6,25 @@ import Card from './components/Card'
 function App() {
   return (
     <div className="App">
-      {data.map((thing, i) => (
+      {console.log(data)}
+      {data.map((item, i) => (
         <Card 
             key = {i}
-
-
-
-            
+            cardPrice = {item.price}
+            cardImage = {item.image}
+            cardLink = {item.linkTo}
+            cardDate = {item.postedOn}
+            cardTitle = {item.title}
+            cardBeds = {item.beds !== 0 ? item.beds : ''}
+            bedExists = {item.beds !== 0 ? 'br ' : ''}
+            cardSize = {item.sqft > 0 ? ` - ${item.sqft}` : ''}
+            sizeExists = {item.sqft > 0 ? 'sf' : ''}
+            sfExists = {item.sqft > 0 ? '2' : ''}
+            cardLocation = {item.location}
         />
       ))}
-      
     </div>
-  );
+  )
 }
 
 export default App;
--- craigslist/src/components/Card.js
+++ craigslist/src/components/Card.js
@@ -3,26 +3,19 @@ import React from 'react'
 const Card = (props) => {
     return (
         <div className="card-box">
-            <div className="card-price">{props.cardPrice}</div>
-            <img className="card-image" src={require(props.cardImage).default}>
-                <a className="card-link" src={require(props.cardLink)}></a>
-            </img>
+            <div className="card-price">${props.cardPrice}</div>
+            <a href={props.cardLink}><img className="card-image" src={props.cardImage} alt={props.cardTitle}></img></a>   
             <div className="card-footer">
-                <span className="footer-date">{props.cardDate}</span>
-                <a className="footer-link" href={require(props.cardLink).default}>{props.cardTitle}</a>
-                <span className="footer-price">{props.cardPrice}</span>
-                <span className="footer-beds">{props.cardBeds}</span>
-                <span className="footer-size">{props.cardSize}</span>
-                <span className="footer-location">{props.cardLocation}</span>
+                <span className="footer-date">{props.cardDate} </span>
+                <a className="footer-link" href={props.cardLink}>{props.cardTitle}</a><span>  </span>
+                <span className="footer-price">${props.cardPrice}</span><span>  </span>
+                <span className="footer-beds">{props.cardBeds}{props.bedExists}</span>
+                <span className="footer-size">{props.cardSize}{props.sizeExists}<sup>{props.sfExists}</sup></span>
+                <span className="footer-location">{` - (${props.cardLocation})`} </span>
             </div>
-
         </div>
     )
 }
-
-
-
-
 
 
 export default Card
